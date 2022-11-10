4c17238645968c2b4cdd366650043a6b3459015c
kanaheo <stalvede0421@gmail.com>
2022-11-10 16:27:51 +0900

    common app create && rooms app, users app models update

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from common.models import CommonModel
 
-# Create your models here.
+class Room(CommonModel):
+    
+    ### Room Model Definition ###
+    
+    class RoomKindChoices(models.TextChoices):
+        ENTIRE_PLACE = ("entire_place", "Entire Place")
+        PRIVATE_ROOM = ("private_room", "Private Room")
+        SHARED_ROOM = ("shared_room", "Shared Room")
+    
+    country = models.CharField(max_length=50, default="korea")
+    city = models.CharField(max_length=80, default="seoul")
+    price = models.PositiveIntegerField()
+    rooms = models.PositiveIntegerField()
+    toilets = models.PositiveIntegerField()
+    description = models.TextField()
+    address = models.CharField(max_length=250)
+    pet_friendly = models.BooleanField(default=True)
+    kind = models.CharField(max_length=20, choices=RoomKindChoices.choices)
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    amenities = models.ManyToManyField("rooms.Amenity")
+
+
+class Amenity(CommonModel):
+    
+    """ Amenity Definition """
+    
+    name = models.CharField(max_length=150)
+    description = models.CharField(
+        max_length=150, 
+        null=True, # 이건 DB가 null가능한거
+        blank=True # 이건 장고 어드민 패널에서 빈값이 가능하게
+    )
