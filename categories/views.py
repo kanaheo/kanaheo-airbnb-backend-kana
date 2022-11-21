58d4f659cf40214ca020d6afd34cc00d92cd08fc
kanaheo <stalvede0421@gmail.com>
2022-11-21 18:18:28 +0900

    django serializer data valid check

--- categories/views.py
+++ categories/views.py
@@ -3,13 +3,33 @@ from rest_framework.response import Response
 from .models import Category
 from .serializers import CategorySerializer
 
-@api_view()
+@api_view(["GET", "POST"])
 def categories(request):
-    all_categories = Category.objects.all()
-    serializer = CategorySerializer(all_categories, many=True)
+    
+    if request.method == "GET":
+        all_categories = Category.objects.all()
+        serializer = CategorySerializer(all_categories, many=True)
+        return Response(
+            serializer.data,
+        )
+    elif request.method == "POST":
+        serializer = CategorySerializer(data=request.data)
+        # print(serializer.is_valid())
+        # print(serializer.errors)
+        if serializer.is_valid():
+            return Response(
+                {
+                    "created": True
+                }
+            )
+        else:
+            return Response(serializer.errors)
+    
+    
+@api_view()
+def category(request, pk):
+    category = Category.objects.get(pk=pk)
+    serializer = CategorySerializer(category)
     return Response(
-        {
-            "ok": True,
-            "categories": serializer.data,
-        }
+        serializer.data,
     )
