19ebc83b7a7a2d867a64283a1761bb3688783cdd
kanaheo <stalvede0421@gmail.com>
2022-11-22 15:21:18 +0900

    url reset

--- categories/urls.py
+++ categories/urls.py
@@ -1,10 +1,18 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from . import views
 
-router = DefaultRouter()
-router.register("", views.CategoryViewSet, basename="categories")
-
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", views.CategoryViewSet.as_view(
+        {
+            "get": "list",
+            "post": "create",  
+        }
+    )),
+    path("<int:pk>", views.CategoryViewSet.as_view(
+        {
+            "get": "retrieve",
+            "put": "partial_update",
+            "delete": "destroy",
+        }
+    ))
 ]
