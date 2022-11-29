b54672c2028b5a15ac99695a3aedaaef98009f5c
kanaheo <stalvede0421@gmail.com>
2022-11-29 10:59:34 +0900

    room amenities api end

--- rooms/views.py
+++ rooms/views.py
@@ -193,3 +193,29 @@ class RoomReviews(APIView):
             many=True
         )
         return Response(serializer.data)
+    
+class RoomAmenities(APIView):
+    
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        try:
+            page = int(request.query_params.get("page", 1))
+        except ValueError:
+            page = 1
+        
+        page_size = 5
+        start = (page-1) * page_size
+        end = start + page_size
+        
+        room = self.get_object(pk)
+        serializer = AmenitySerializer(
+            room.amenities.all()[start:end],
+            many=True
+        )
+        
+        return Response(serializer.data)
