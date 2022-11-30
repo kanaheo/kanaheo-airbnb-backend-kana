035d1187f331bae83c8af6949da7f163075059ce
kanaheo <stalvede0421@gmail.com>
2022-11-30 12:39:16 +0900

    rooms's reviews serialzers end

--- rooms/views.py
+++ rooms/views.py
@@ -173,6 +173,8 @@ class RoomDetail(APIView):
 
 class RoomReviews(APIView):
     
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
     def get_object(self, pk):
         try:
             return Room.objects.get(pk=pk)
@@ -194,6 +196,16 @@ class RoomReviews(APIView):
             many=True
         )
         return Response(serializer.data)
+    
+    def post(self, request, pk):
+        serializer = ReviewSerializer(data=request.data)
+        if serializer.is_valid():
+            review = serializer.save(
+                user=request.user,
+                room=self.get_object(pk)
+            )
+            serializer = ReviewSerializer(review)
+            return Response(serializer.data)
     
 class RoomAmenities(APIView):
     
