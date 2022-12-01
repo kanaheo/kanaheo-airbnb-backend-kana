49e1c941a553b0850fee4efe5e4bb9488ea8aaef
kanaheo <stalvede0421@gmail.com>
2022-12-01 15:25:32 +0900

    wishlist rooms add !

--- wishlists/views.py
+++ wishlists/views.py
@@ -4,6 +4,7 @@ from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models import Wishlist
+from rooms.models import Room
 from .serializers import WishlistSerializer
 
 class Wishlists(APIView):
@@ -45,6 +46,7 @@ class WishlistDetail(APIView):
         wishlist = self.get_object(pk, request.user)
         serializer = WishlistSerializer(
             wishlist,
+            context={"request": request}
         )
         return Response(serializer.data)
     
@@ -59,13 +61,39 @@ class WishlistDetail(APIView):
             wishlist,
             data=request.data,
             partial=True,
+            context={"request": request}
         )
         
         if serializer.is_valid():
             wishlist = serializer.save()
             serializer = WishlistSerializer(
                 wishlist,
+                context={"request": request}
             )
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+        
+class WishlistRoomToogle(APIView):
+    
+    def get_wishlist(self, pk, user):
+        try:
+            return Wishlist.objects.get(pk=pk, user=user)
+        except Wishlist.DoesNotExist:
+            raise NotFound
+    
+    def get_room(self, pk): # pk는 put이나 delete에서 room_pk를 뜻함
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+    
+    def put(self, request, pk, room_pk):
+        wishlist = self.get_wishlist(pk, request.user)
+        room = self.get_room(room_pk)
+        
+        if wishlist.rooms.filter(pk=room.pk).exists():  # wishlist에서 기존에 있는 room 지우기
+            wishlist.rooms.remove(room)
+        else: # wishlist에 room추가하기
+            wishlist.rooms.add(room)
+        return Response(status=HTTP_200_OK)
