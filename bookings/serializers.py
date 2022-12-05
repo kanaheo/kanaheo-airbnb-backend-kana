76f9072837b68750e2b4425ae52bfa6528e075e7
kanaheo <stalvede0421@gmail.com>
2022-12-05 17:21:21 +0900

    room booking validate check_in, check_out not past date

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -1,5 +1,34 @@
+from django.utils import timezone
 from rest_framework import serializers
 from .models import Booking
+
+# only create room booking create api serializer
+class CreateRoomBookingSerializer(serializers.ModelSerializer):
+    
+    check_in = serializers.DateField()  # defulut required value
+    check_out = serializers.DateField() # defulut required value
+    
+    class Meta:
+        model = Booking
+        fields = (
+            "check_in",
+            "check_out",
+            "guests",
+        )
+    
+    def validate_check_in(self, value):
+        now = timezone.localtime(timezone.now()).date()
+        if now > value:
+            raise serializers.ValidationError("Can't book in the past!")
+        else:
+            return value
+        
+    def validate_check_out(self, value):
+        now = timezone.localtime(timezone.now()).date()
+        if now > value:
+            raise serializers.ValidationError("Can't book in the past!")
+        else:
+            return value
 
 
 # 이건 모두가 보는 것
--- rooms/views.py
+++ rooms/views.py
@@ -12,7 +12,7 @@ from .serializers import AmenitySerializer, RoomListSerializer, RoomDetailSerial
 from reviews.serializers import ReviewSerializer
 from medias.serializers import PhotoSerializer
 from bookings.models import Booking
-from bookings.serializers import PublicBookingSerializer
+from bookings.serializers import PublicBookingSerializer, CreateRoomBookingSerializer
 
 class Amenities(APIView):
     
@@ -278,3 +278,12 @@ class RoomBookings(APIView):
         )
         serializer = PublicBookingSerializer(bookings, many=True)
         return Response(serializer.data)
+    
+    def post(self, request, pk):
+        room = self.get_object(pk)
+        serializer = CreateRoomBookingSerializer(data=request.data)
+        if serializer.is_valid():
+            check_in = request.data.get("check_in")
+            pass
+        else:
+            return Response(serializer.errors)
