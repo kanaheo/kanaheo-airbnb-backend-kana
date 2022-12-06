297d93437af49ea06c1ac4b8c07837542412f68b
kanaheo <stalvede0421@gmail.com>
2022-12-06 10:47:37 +0900

    create user!

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.exceptions import ParseError
 from rest_framework.permissions import IsAuthenticated
 from . import serializers
 
@@ -26,4 +27,22 @@ class Me(APIView):
             serializer = serializers.PrivateUserSerializer(user)
             return Response(serializer.data)
         else:
-            return Response(serializers.errors)
+            return Response(serializer.errors)
+
+class Users(APIView):
+    def post(self, request):
+        password = request.data.get("password")
+        if not password:
+            raise ParseError
+        serializer = serializers.PrivateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            pass
+            # 아직 여기까진 패스워드를 가지고 있지 않다 !! 따로 설정해줘야함 ! 
+            user = serializer.save()    # 일단 유저는 생성 하지만 아직 password는 생성 X
+            # user.password = password <- X 왜냐하면 받은 글자 그대로 패스워드에 저장함 ;; 그래서 아래처럼 하자
+            user.set_password(password)
+            user.save()
+            serializer = serializers.PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
