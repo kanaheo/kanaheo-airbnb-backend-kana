951f3612991422db22cbf5a5a097b632341ab414
kanaheo <stalvede0421@gmail.com>
2022-12-12 23:56:15 +0900

    rooms schema & queries end

--- config/schema.py
+++ config/schema.py
@@ -1,40 +1,15 @@
 import strawberry
-import typing
-
-@strawberry.type
-class Movie:
-    pk:int
-    title:str
-    year:int
-    rating:int
-
-movies_db = [
-    Movie(pk=1,title="godfather", year=1990, rating=10),
-]
-
-def add_movie(title: str, year: int, rating: int) -> Movie:
-    new_movie = Movie(
-        pk=len(movies_db)+1,
-        title=title,
-        year=year,
-        rating=rating
-    )
-    movies_db.append(new_movie)
-    return new_movie
-
-def movies() -> typing.List[Movie]:
-    return movies_db
-
-def movie(movie_pk:int) -> Movie:
-    return movies_db[movie_pk-1]
+from rooms import schema as rooms_schema
 
 @strawberry.type
 class Mutation:
-    add_movie: Movie = strawberry.mutation(resolver=add_movie)
+    pass
 
 @strawberry.type
-class Query:
-    movies: typing.List[Movie] = strawberry.field(resolver=movies)
-    movie: Movie = strawberry.field(resolver=movie)
+class Query(rooms_schema.Query):
+    pass
 
-schema = strawberry.Schema(query=Query, mutation=Mutation)
+schema = strawberry.Schema(
+    query=Query,
+    # mutation=Mutation
+)
