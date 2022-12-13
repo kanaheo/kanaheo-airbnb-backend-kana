853944a412452a54fc6efbe2af9b7d53ba447dac
kanaheo <stalvede0421@gmail.com>
2022-12-13 12:07:30 +0900

    graphql rooms related users models

--- rooms/types.py
+++ rooms/types.py
@@ -1,9 +1,11 @@
 import strawberry
 from strawberry import auto
 from . import models
+from users.types import UserType
 
 @strawberry.django.type(models.Room)
 class RoomType:
     id: auto
     name: auto
     kind: auto
+    owner: "UserType"
