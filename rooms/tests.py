9c1b2faf203c009eda5f1d2e2cabcd9cd988bd1f
kanaheo <stalvede0421@gmail.com>
2022-12-14 11:14:39 +0900

    amenity put api test end

--- rooms/tests.py
+++ rooms/tests.py
@@ -77,5 +77,104 @@ class TestAmenities(APITestCase):
             400
         )
         self.assertIn("name", data)
+
+class TestAmenity(APITestCase):
+    
+    NAME = "Test Amenity"
+    DESC = "Test Dsc"
+    
+    def setUp(self):
+        models.Amenity.objects.create(
+            name=self.NAME,
+            description=self.DESC,
+        )
+    
+    def test_amenity_not_found(self):
+        response = self.client.get("/api/v1/rooms/amenities/2") # 1개에 대해서 테스트 하니까 ! pk=1이 된다 ! 하지만 이건 404 테스트 ! 잘 404가 되는지
         
+        self.assertEqual(response.status_code, 404, "Page is Exists!")
+    
+    def test_get_amenity(self):
+        
+        response = self.client.get("/api/v1/rooms/amenities/1") # 1개에 대해서 테스트 하니까 ! pk=1이 된다 ! 테스트 데이터는 매번 생성하면 삭제되니까
+        
+        self.assertEqual(response.status_code, 200, "Page is not exists")
+        
+        data = response.json()
+        
+        self.assertEqual(data["name"], self.NAME)
+        self.assertEqual(data["description"], self.DESC)
+    
+    def test_put_amenity(self):
+        #setUp을 1개 해주니까 바로 update를 들어가도 된다.
+        
+        # update sucess start #
+        update_amenity_name = "update Amenity"
+        update_amenity_description = "update Amenity Desc"
+        
+        response = self.client.put(
+            "/api/v1/rooms/amenities/1",
+            data={
+                "name": update_amenity_name,
+                "description": update_amenity_description,
+            }
+        )
+        data = response.json()
+        
+        self.assertEqual(
+            response.status_code,
+            200,
+            "Not 200 status code",
+        )
+        
+        self.assertEqual(
+            data["name"], update_amenity_name
+        )
+        
+        self.assertEqual(
+            data["description"], update_amenity_description
+        )
+        # update sucess end #
+        
+        # update wrong name start #
+        update_amenity_name = "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"
+        response = self.client.put(
+            "/api/v1/rooms/amenities/1",
+            data={
+                "name": update_amenity_name,
+            }
+        )
+        
+        self.assertEqual(response.status_code, 400)
+        # update wrong name end #
+        
+        # update name empty start #
+        response = self.client.put(
+            "/api/v1/rooms/amenities/1",
+            data={
+                "name": ""
+            }
+        )
+        data = response.json()
+        
+        self.assertEqual(response.status_code, 400)
+        # update name empty end #
+        
+        # update wrong desc start #
+        update_amenity_description = "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"
+        response = self.client.put(
+            "/api/v1/rooms/amenities/1",
+            data={
+                "description": update_amenity_description,
+            }
+        )
+        
+        self.assertEqual(response.status_code, 400)
+        # update wrong desc end #
+    
+    def test_delete_amenity(self):
+        
+        response = self.client.delete("/api/v1/rooms/amenities/1")
+        
+        self.assertEqual(response.status_code, 204)
         
