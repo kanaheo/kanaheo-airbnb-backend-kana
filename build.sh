284b662ffc68576886b5820b55aa2d242509f49b
kanaheo <stalvede0421@gmail.com>
2022-12-20 17:17:11 +0900

    env & build.sh edit2

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+pip install --upgrade pip
 
 poetry install
-pip install --upgrade pippip install --force-reinstall -U setuptools
+pip install --force-reinstall -U setuptools
 
 python manage.py collectstatic --no-input
 python manage.py migrate
