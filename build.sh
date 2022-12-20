7d292759f3334765fff6a9203636c59520ef4646
kanaheo <stalvede0421@gmail.com>
2022-12-20 16:45:44 +0900

    env & build.sh edit2

--- build.sh
+++ build.sh
@@ -4,10 +4,5 @@ set -o errexit
 
 poetry install
 
-# pip install --upgrade pip pippip install --force-reinstall -U setuptools
-pip install --upgrade pip
-pip install --force-reinstall -U setuptools
-pippip install --force-reinstall -U setuptools
-
 python manage.py collectstatic --no-input
 python manage.py migrate
