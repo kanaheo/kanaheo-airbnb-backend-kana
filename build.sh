401bfac4d05bd5207ed1b6c6e1336af9a7bb5cc2
kanaheo <stalvede0421@gmail.com>
2022-12-20 16:07:07 +0900

    env & build.sh edit

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 # exit on error
 set -o errexit
 
-poetry install
+#poetry install
+
+pip install --upgrade pippip install --force-reinstall -U setuptools
 
 python manage.py collectstatic --no-input
 python manage.py migrate
