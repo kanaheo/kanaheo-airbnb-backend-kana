ee107ea7549c0b4e10d054bf886fdb7ba425200a
kanaheo <stalvede0421@gmail.com>
2022-12-25 23:39:27 +0900

    test

--- config/settings.py
+++ config/settings.py
@@ -13,10 +13,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import environ
-import dj_database_url
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
-
+import dj_database_url
 
 
 env = environ.Env()
@@ -34,14 +33,15 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = [
     "localhost",
-    "backend.kknkkm.site/",
+    "backend.kknkkm.site",
 ]
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')   # render가 외부로 노출시키는 url을 의미
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+# render가 외부로 노출시키는 url을 의미
 # render로 배포를 하면 랜덤 domain을 얻는다 ! 그 domain을 위에 다가 넣어줄것이다 ! 
 # 그리고 이 밑에서는 그것을 허용해주겠당 ! (위에게 존재하면 !)
 if RENDER_EXTERNAL_HOSTNAME:
@@ -65,23 +65,23 @@ CUSTOM_APPS = [
     "wishlists.apps.WishlistsConfig",
     "bookings.apps.BookingsConfig",
     "medias.apps.MediasConfig",
-    "direct_messages.apps.DirectMessagesConfig"
+    "direct_messages.apps.DirectMessagesConfig",
 ]
 
-SYSTEMAPPS_APPS = [
+SYSTEM_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles"
+    "django.contrib.staticfiles",
 ]
 
-INSTALLED_APPS = SYSTEMAPPS_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
+INSTALLED_APPS = SYSTEM_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -122,11 +122,11 @@ if DEBUG:
             "NAME": BASE_DIR / "db.sqlite3",
         }
     }
-else:
+else:      # 이건 DB연결이 종료되기 전 연결 유지 시간을 뜻 ! 하나의 연결에 너무 많은 시간을 사용하고 싶지 않다 ! timeout 같은 !
     DATABASES = {
         "default": dj_database_url.config(
-            conn_max_age=600,   # 이건 DB연결이 종료되기 전 연결 유지 시간을 뜻 ! 하나의 연결에 너무 많은 시간을 사용하고 싶지 않다 ! timeout 같은 !
-        ),
+            conn_max_age=600,
+        )
     }
 
 
@@ -165,11 +165,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -198,7 +199,7 @@ REST_FRAMEWORK = {
         "config.authentication.TrustMeBroAuthentication",
         "rest_framework.authentication.TokenAuthentication",
         "config.authentication.JWTAuthentication",
-    ]
+    ],
 }
 
 if DEBUG:
@@ -210,10 +211,7 @@ else:
     CORS_ALLOWED_ORIGINS = ["https://kknkkm.site"]
     CSRF_TRUSTED_ORIGINS = ["https://kknkkm.site"]
 
-# secret key = RRRej24SKnxvyjNRu0ZPZnEgap8n9J3m
-
 CORS_ALLOW_CREDENTIALS = True
-
 GH_SECRET = env("GH_SECRET")
 
 CF_ID = env("CF_ID")
@@ -230,5 +228,5 @@ if not DEBUG:
             DjangoIntegration(),
         ],
         traces_sample_rate=1.0,
-        send_default_pii=True
+        send_default_pii=True,
     )
