a0edf95510caf281e6ebdfed4a7275075910a1e2
kanaheo <stalvede0421@gmail.com>
2022-12-27 18:04:42 +0900

    csrf

--- config/settings.py
+++ config/settings.py
@@ -206,8 +206,8 @@ if DEBUG:
     CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000", "http://localhost:3000"]
     CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000"]
 else:
-    CORS_ALLOWED_ORIGINS = ["https://kkanakana-frontend.onrender.com"]
-    CSRF_TRUSTED_ORIGINS = ["https://kkanakana-frontend.onrender.com"]
+    # CORS_ALLOWED_ORIGINS = ["https://kkanakana-frontend.onrender.com"]
+    # CSRF_TRUSTED_ORIGINS = ["https://kkanakana-frontend.onrender.com"]
     CORS_ALLOWED_ORIGINS = ["https://kknkkm.site"]
     CSRF_TRUSTED_ORIGINS = ["https://kknkkm.site"]
 
